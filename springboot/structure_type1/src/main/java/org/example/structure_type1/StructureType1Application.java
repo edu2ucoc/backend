3df6b012190a3e29f07b91a903ead30666af0199
edu2ucoc <edu2.ucoc@gmail.com>
2025-01-05 18:28:43 +0900

    Feat: 스프링부트 모놀리식 타입1 - SSR 단독 샘플

---
+++ springboot/structure_type1/src/main/java/org/example/structure_type1/StructureType1Application.java
@@ -0,0 +1,13 @@
+package org.example.structure_type1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructureType1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructureType1Application.class, args);
+    }
+
+}
---
+++ springboot/structure_type1/src/test/java/org/example/structure_type1/StructureType1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.structure_type1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructureType1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
